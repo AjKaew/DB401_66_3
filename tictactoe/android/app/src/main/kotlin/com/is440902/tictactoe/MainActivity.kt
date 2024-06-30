6233c761a84df90bda4d5dac3bd1d6d3713c8a45
AjKaew <chaloemphon.sir@dpu.ac.th>
2024-06-30 16:39:00 +0700

    Tic-Tac-Toe Pert1

---
+++ tictactoe/android/app/src/main/kotlin/com/is440902/tictactoe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.is440902.tictactoe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ tictactoe/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'pages/challenge.dart';
+import 'pages/login.dart';
+import 'pages/register.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        'login': (context) => const Login(),
+        'register': (context) => const Register(),
+        'challenge': (context) => const Challenge(),
+      },
+      initialRoute: 'login',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.lightBlueAccent,
+          foregroundColor: Colors.white
+        ),
+        filledButtonTheme: FilledButtonThemeData(
+          style: FilledButton.styleFrom(
+            backgroundColor: Colors.blue
+          )
+        )
+      ),
+    );
+  }
+}
